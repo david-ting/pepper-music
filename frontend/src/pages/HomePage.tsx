d1f01090886e77672f9a83ff0de2eef66efde10a
david-ting <davidting159@gmail.com>
2021-01-17 20:48:46 +0800

    improve UI of HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,6 +3,8 @@ import Container from "react-bootstrap/Container";
 import { IconContext } from "react-icons";
 import { FaSpotify } from "react-icons/fa";
 import loginHandler from "../customFunc/loginHandler";
+import musicSVG from "../images/music.svg";
+import MusicBars from "../components/MusicBars";
 
 const HomePage: React.FC = () => {
   document.title = "Pepper Music";
@@ -10,6 +12,12 @@ const HomePage: React.FC = () => {
   return (
     <Container fluid className="outer-container">
       <Container className="center-container">
+        <div id="music-person-container">
+          <img src={musicSVG} alt="music" id="music-person" />
+          <div>
+            <MusicBars />
+          </div>
+        </div>
         <div id="loginPrompt" onClick={loginHandler}>
           <h3>
             <span>
